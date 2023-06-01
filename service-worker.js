8dc00c359e6d55529f3a3625fca1c8ffaf5bd782
Lisa Uloh <lissyuloh8@gmail.com>
2023-06-01 16:26:44 +0400

    code for it to save and download

--- service-worker.js
+++ service-worker.js
@@ -21,11 +21,25 @@ self.addEventListener('install', (e) => {
     );
 });
 
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resources', e.request.url);
+//             return r
+//         })
+//     );
+    
+// });
+
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r) {
-            console.log('[Service Worker] Fetching resources', e.request.url);
-            return r
+            return r || fetch(e.request).then(function (response) {
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            })
         })
     );
     
