a973a09f182cf3ebee77b14dcb6e3d95c2e4eca6
Lisa Uloh <lissyuloh8@gmail.com>
2023-06-01 16:00:01 +0400

    PWA

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+var cacheName = "petstore-v1";
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'petstore.webmanifest',
+    'images/1.jpg',
+    'images/2.jpg',
+    'images/3.jpg',
+    'images/4.jpg',
+    'images/5.jpg',
+    'images/icon-store-512.png',
+]
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching app shell');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            console.log('[Service Worker] Fetching resources', e.request.url);
+            return r
+        })
+    );
+    
+});
